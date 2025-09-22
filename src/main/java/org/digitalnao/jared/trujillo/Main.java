b7cac8ef5834059c4e91edafe76d3349a37a468f
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2025-09-22 17:32:51 -0600

    Main implementation

--- src/main/java/org/digitalnao/jared/trujillo/Main.java
+++ src/main/java/org/digitalnao/jared/trujillo/Main.java
@@ -1,7 +1,33 @@
 package org.digitalnao.jared.trujillo;
 
+import org.digitalnao.jared.trujillo.handlers.CsvHandlerFactory;
+import org.digitalnao.jared.trujillo.handlers.JsonHandlerFactory;
+import org.digitalnao.jared.trujillo.classes.User;
+import org.digitalnao.jared.trujillo.interfaces.CsvHandler;
+import org.digitalnao.jared.trujillo.interfaces.JsonHandler;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        JsonHandler jsonHandler = JsonHandlerFactory.createJsonHandler();
+        CsvHandler csvHandler = CsvHandlerFactory.createCsvHandler();
+
+        try {
+            User user = jsonHandler.fromJson("user.json", User.class);
+            List<User> userList = jsonHandler.fromJsonList("users-list.json", User.class);
+            System.out.println(user.toString());
+
+            for (User userFromList : userList) {
+                System.out.println(userFromList.toString());
+            }
+
+            csvHandler.writeToCsv(user, User.class, "user");
+            csvHandler.writeToCsv(userList, User.class, "users");
+        } catch(Exception e) {
+            System.err.println(e);
+        }
 
     }
 }
